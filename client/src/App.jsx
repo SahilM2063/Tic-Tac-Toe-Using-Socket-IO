4c69ea515fe7bf5a23c5f128daa502fdfacb3451
Sahil Makadia <makadiasahil9846@gmail.com>
2024-02-06 23:03:46 +0530

    Basic Game Functionality and Game Mechanics Added , also UI Added for Wining and Final State

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import tictactoeBg from "./assets/tictactoe-bg.svg";
 import SquareBox from "./components/SquareBox";
 
@@ -11,27 +11,117 @@ const arrItems = [
 
 function App() {
   const [gameState, setGameState] = useState(arrItems);
+  const [currentPlayer, setCurrentPlayer] = useState("circle");
+  const [finishedState, setFinishedState] = useState(false);
+  const [finishedArray, setFinishedArray] = useState([]);
+
+  const checkWinner = () => {
+    // dynamic row and static column
+    for (let row = 0; row < gameState.length; row++) {
+      if (
+        gameState[row][0] === gameState[row][1] &&
+        gameState[row][1] === gameState[row][2]
+      ) {
+        setFinishedArray([row * 3, row * 3 + 1, row * 3 + 2]);
+        return gameState[row][0];
+      }
+    }
+
+    // dynamic column and static row
+    for (let col = 0; col < gameState.length; col++) {
+      if (
+        gameState[0][col] === gameState[1][col] &&
+        gameState[1][col] === gameState[2][col]
+      ) {
+        setFinishedArray([0 * 3 + col, 1 * 3 + col, 2 * 3 + col]);
+        return gameState[0][col];
+      }
+    }
+
+    // cross winning moves
+    if (
+      gameState[0][0] === gameState[1][1] &&
+      gameState[1][1] === gameState[2][2]
+    ) {
+      setFinishedArray([0,4,8]);
+      return gameState[0][0];
+    }
+    
+    if (
+      gameState[0][2] === gameState[1][1] &&
+      gameState[1][1] === gameState[2][0]
+      ) {
+      setFinishedArray([2,4,6]);
+      return gameState[0][2];
+    }
+
+    const isDrawMatch = gameState.flat().every((e) => {
+      if (e === "circle" || e === "cross") return true;
+    });
+
+    if (isDrawMatch) {
+      return "draw";
+    }
+    console.log(isDrawMatch);
+
+    return null;
+  };
+
+  useEffect(() => {
+    const winner = checkWinner();
+    if (winner) {
+      setFinishedState(winner);
+      console.log(winner);
+    }
+  }, [gameState]);
 
   return (
-    <div className="w-screen h-screen bg-[#43115B] relative flex justify-center items-center">
+    <div className="w-screen h-screen bg-[#43115B] relative flex flex-col justify-center items-center">
       <img
         src={tictactoeBg}
         alt="tictactoeBg"
         className="absolute left-0 bottom-0 w-[22vh] xs:w-[16vh] sm:w-[18vh] md:w-[20vh]"
       />
+      {finishedState &&
+        (finishedState === "draw" ? (
+          <h3 className="text-white font-Gilroy mb-2">
+            Match is
+            <span className="text-2xl font-medium capitalize">
+              {" "}
+              {finishedState} 👁️💫
+            </span>
+          </h3>
+        ) : (
+          <h3 className="text-white font-Gilroy mb-2">
+            <span className="text-2xl font-medium capitalize">
+              {finishedState}{" "}
+            </span>
+            Won The Game! 💫✨
+          </h3>
+        ))}
       <div className="container w-full min-w-md xs:w-[96%] md:w-[80%] lg:w-[40%] xl:w-[40%] 2xl:w-[30%] bg-[#2B0040] rounded-2xl flex flex-col justify-center items-center py-2">
         <div className="turn-board w-full grid grid-cols-2 place-content-between gap-2 xs:px-4 sm:px-6 md:px-12 lg:px-12 xl:px-12 2xl:px-10 mt-4">
-          <div className="tag p-3 bg-[#48D2FE]  rounded-lg text-center font-Gilroy font-bold text-lg">
+          <div className="tag p-3 bg-[#E2BE00] rounded-lg text-center font-Gilroy font-bold text-lg">
             You
           </div>
-          <div className="tag p-3 bg-[#E2BE00]  rounded-lg text-center font-Gilroy font-bold text-lg">
+          <div className="tag p-3 bg-[#48D2FE]  rounded-lg text-center font-Gilroy font-bold text-lg">
             Opponent
           </div>
         </div>
         <div className="game-board w-full grid grid-cols-3 grid-rows-3 gap-4 place-items-center xs:px-4 sm:px-6 md:px-14 lg:px-12 xl:px-16 2xl:px-20 xs:py-4 sm:py-4 md:py-8 lg:py-10 xl:py-10 2xl:p-10">
-          {gameState.map((arr) => {
-            return arr.map((e) => {
-              return <SquareBox setGameState={setGameState} key={e} id={e} />;
+          {gameState.map((arr, rowIndex) => {
+            return arr.map((e, colIndex) => {
+              return (
+                <SquareBox
+                  finishedArray={finishedArray}
+                  setGameState={setGameState}
+                  currentPlayer={currentPlayer}
+                  setCurrentPlayer={setCurrentPlayer}
+                  finishedState={finishedState}
+                  key={rowIndex * 3 + colIndex}
+                  id={rowIndex * 3 + colIndex}
+                />
+              );
             });
           })}
         </div>
--- client/src/components/SquareBox.jsx
+++ client/src/components/SquareBox.jsx
@@ -1,14 +1,99 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import circle_icon from "../assets/circle_icon.svg";
-import cross_icon from "../assets/cross_icon.svg";
 
-const SquareBox = ({ setGameState, id }) => {
-  const [icon, setIcon] = useState(cross_icon);
+const circle_icon = (
+  <svg viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+        stroke="#E2BE00"
+        strokeWidth="2.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </g>
+  </svg>
+);
+
+const cross_icon = (
+  <svg
+    fill="#48D2FE"
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 92 92"
+    enableBackground="new 0 0 92 92"
+    xmlSpace="preserve"
+    stroke="#48D2FE"
+    strokeWidth="6.256"
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        id="XMLID_732_"
+        d="M70.7,64.3c1.8,1.8,1.8,4.6,0,6.4c-0.9,0.9-2,1.3-3.2,1.3c-1.2,0-2.3-0.4-3.2-1.3L46,52.4L27.7,70.7 c-0.9,0.9-2,1.3-3.2,1.3s-2.3-0.4-3.2-1.3c-1.8-1.8-1.8-4.6,0-6.4L39.6,46L21.3,27.7c-1.8-1.8-1.8-4.6,0-6.4c1.8-1.8,4.6-1.8,6.4,0 L46,39.6l18.3-18.3c1.8-1.8,4.6-1.8,6.4,0c1.8,1.8,1.8,4.6,0,6.4L52.4,46L70.7,64.3z"
+      ></path>
+    </g>
+  </svg>
+);
+
+const SquareBox = ({
+  setGameState,
+  id,
+  setCurrentPlayer,
+  currentPlayer,
+  finishedState,
+  finishedArray,
+}) => {
+  const [icon, setIcon] = useState(null);
+
+  const clickOnBox = () => {
+    if (finishedState) {
+      return;
+    }
+
+    if (!icon) {
+      if (currentPlayer === "circle") {
+        setIcon(circle_icon);
+      } else {
+        setIcon(cross_icon);
+      }
+
+      const myCurrentPlayer = currentPlayer;
+      setCurrentPlayer(currentPlayer === "circle" ? "cross" : "circle");
+
+      setGameState((prevState) => {
+        let newState = [...prevState];
+        const rowIndex = Math.floor(id / 3);
+        const colIndex = id % 3;
+        newState[rowIndex][colIndex] = myCurrentPlayer;
+        return newState;
+      });
+    }
+  };
+
   return (
-    <div className="bg-[#43115B] rounded-lg flex flex-col justify-center items-center cursor-pointer text-white w-[80px] h-[80px] md:w-[90px] md:h-[90px]">
-      <img src={icon} alt="icon" className="w-[60%]" />
+    <div
+      onClick={clickOnBox}
+      className={`box bg-[#43115B] rounded-lg flex flex-col justify-center items-center cursor-pointer text-white w-[80px] h-[80px] md:w-[90px] md:h-[90px] ${
+        finishedState ? "not-allowed" : ""
+      } ${finishedArray.includes(id) ? "bg-[#FFF]" : ""}`}
+    >
+      {icon}
     </div>
   );
 };
